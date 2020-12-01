de5cb35e40d31f1748ffc26a1aab5e2e6e66f9b0
drybin24 <74720460+drybin24@users.noreply.github.com>
2020-12-01 19:11:20 +0300

    to` jjlby ghtrhfcysq ltym

---
+++ New Unity Project/Assets/Scrips/Button_for_new_word.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_for_new_word : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject canvas;
+    public Vector3 brickPosition = new Vector3(10, 0, 0);
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instance = Instantiate(prefab, transform.position + brickPosition, transform.rotation) as GameObject;
+        }
+    }
+
+    public void OnMouseClick ()
+    {
+        Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity, canvas.transform);
+    }
+}

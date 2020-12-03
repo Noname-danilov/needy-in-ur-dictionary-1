1e5427b73a82c08813d51c2bd15a93ed423c4e0f
drybin24 <74720460+drybin24@users.noreply.github.com>
2020-12-03 18:55:26 +0300

    Nothing

---
+++ New Unity Project/Assets/Scrips/Damage_Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System;
+
+public class Damage_Calculator : MonoBehaviour
+{
+    public static int CalculateDamage(int amount, float mitigationPercent)
+    {
+        return Convert.ToInt32(amount * mitigationPercent);
+        Assert.AreEqual(5, final);
+    }
+}

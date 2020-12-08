19f8ddaaafdffda76cf201eda71c1171160c94a7
drybin24 <74720460+drybin24@users.noreply.github.com>
2020-12-08 19:30:45 +0300

    forgotGoodAdding...

--- New Unity Project/Assets/Scrips/ImageControl.cs
+++ New Unity Project/Assets/Scrips/ImageControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ImageControl : MonoBehaviour
 {
+    public Sprite image1;
+    public Sprite image2;
+    public Sprite image3;
+    public Sprite image4;
     // Start is called before the first frame update
     void Start()
     {
